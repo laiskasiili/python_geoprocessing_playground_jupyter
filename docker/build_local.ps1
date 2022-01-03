54b0076c920d0d3ae34bef8614ea5d056de76e74
laiskaSiili <marc.folini@gmail.com>
2022-01-03 14:08:49 +0100

    Initial dockerfile with local build script

---
+++ docker/build_local.ps1
@@ -0,0 +1,7 @@
+# Running scripts is deactivated by default in Powershell. First time use,
+# run as admin in powershell: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+# Remove potentially existing images
+docker rmi $(docker images 'pythongeopprocessingplayground' -q)
+# Build new imag, use content of folder ./ctx as context
+docker build -t pythongeopprocessingplayground:latest .
---
+++ docker/start_jupyter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ./data
+#jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser --autoreload
+jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --autoreload
