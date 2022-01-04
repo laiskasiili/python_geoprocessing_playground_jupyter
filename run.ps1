cb9f4372ca6c162dbf7eaf0704a82183af50a5f8
laiskaSiili <marc.folini@gmail.com>
2022-01-04 10:53:14 +0100

    Modify run script to use current git hash as tag

--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-docker run -it --rm -p 8888:8888 --volume="$($(get-location).Path)":"/geo/data" pythongeopprocessingplayground:latest
+docker run -it --rm -p 8888:8888 --volume="$($(get-location).Path)":"/geo/data" pythongeopprocessingplayground:$(git log -1 --pretty=%H)
