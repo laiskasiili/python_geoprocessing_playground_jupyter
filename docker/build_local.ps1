25b4f4d67910e52530baf6b828c261b10929937b
laiskaSiili <marc.folini@gmail.com>
2022-01-04 10:15:52 +0100

    Modify build_local script to use commit hash as tag

--- docker/build_local.ps1
+++ docker/build_local.ps1
@@ -4,4 +4,4 @@
 # Remove potentially existing images
 docker rmi $(docker images 'pythongeopprocessingplayground' -q)
 # Build new imag, use content of folder ./ctx as context
-docker build -t pythongeopprocessingplayground:latest .
+docker build -t pythongeopprocessingplayground:$(git log -1 --pretty=%H) .
