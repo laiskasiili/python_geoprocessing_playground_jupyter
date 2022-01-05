586b3fb8bedee61c14cde891049825466ee398fb
laiskaSiili <marc.folini@gmail.com>
2022-01-05 09:48:30 +0100

    Clean up repro structure & update gitignore

--- run.sh
+++
@@ -1,4 +0,0 @@
-IMAGE_VERSION_TAG="v0.0.1"
-MOUNT_PATH=$(pwd)
-
-docker run -it --rm -p 8888:8888 --volume="$MOUNT_PATH":"/geo/data" ghcr.io/laiskasiili/python_geoprocessing_playground_jupyter:$IMAGE_VERSION_TAG
--- src/build_local.ps1
+++
@@ -1,7 +0,0 @@
-# Running scripts is deactivated by default in Powershell. First time use,
-# run as admin in powershell: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
-# Remove potentially existing images
-docker rmi $(docker images 'pythongeopprocessingplayground' -q)
-# Build new imag, use content of folder ./ctx as context
-docker build -t pythongeopprocessingplayground:$(git log -1 --pretty=%H) .
